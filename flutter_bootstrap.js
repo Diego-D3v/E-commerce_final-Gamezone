1ad3199a68a14c0198646d8ee4717a8e655407e3
Diego Alexander Diaz <diego.diaz@offimedicas.com>
2026-06-23 16:07:22 -0500

    Built web
    Branch: main
    Commit: b6124dbe270d0162dc090d12b9eae4036448d3b8
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2190263720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3040518571" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
